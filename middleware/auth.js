f9fee55688cb4768bfa17ec7abe3b254a6bebc4f
Shariarx6T9 <najmulhasanshahin@gmail.com>
2025-12-28 01:24:01 +0600

    auth middleware added

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const admin = require('../config/firebase');
+const User = require('../models/User');
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+    
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const user = await User.findOne({ firebaseUID: decodedToken.uid });
+    
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error('Token verification error:', error);
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+const requireRole = (roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: 'Authentication required' });
+    }
+    
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Insufficient permissions' });
+    }
+    
+    next();
+  };
+};
+
+module.exports = { verifyToken, requireRole };
