b83938602b72e4f7b5c207c323746e9590fd7e9f
Shariarx6T9 <najmulhasanshahin@gmail.com>
2025-12-28 01:23:27 +0600

    user model added

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  photoURL: {
+    type: String,
+    default: ''
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'clubManager', 'member'],
+    default: 'admin'
+  },
+  firebaseUID: {
+    type: String,
+    required: true,
+    unique: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
